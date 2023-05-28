01e549ff55db7c9a50c8054161aa77bdfda9aee3
Talya Falach <talyafalach@gmail.com>
2023-05-28 18:08:22 +0300

    .

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <hr className="text-light bg-light" />
 
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<About/>} />
         <Route path="/projects" element={<MyProjects />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery/>} />
